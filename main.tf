28a7ef7be6b5574db3de1421425c357fe5c948e5
Sebastien Langoureaux <sebastien.langoureaux@harmonie-mutuelle.fr>
2018-10-24 13:53:22 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = "${var.location}"
+}
+
+# Get subnet data
+data "aws_subnet" "subnet" {
+  cidr_block = "${var.subnet}"
+}
+
+# Get admin secrurity group
+data "aws_security_group" "sgs" {
+  count = "${length(var.security_group_names)}"
+  tags = {
+    Name = "${element(var.security_group_names, count.index)}"
+  }
+}
+
+# Get centos7 image id
+data "aws_ami" "centos7" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+
+
+# Create repository instance
+module "repository_ec2" {
+  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
+
+  name                        = "${var.name}"
+  instance_count              = "${var.instance_count}"
+  ami                         = "${data.aws_ami.centos7.id}"
+  instance_type               = "${var.instance_type}"
+  key_name                    = "${var.key_name}"
+  monitoring                  = true
+  vpc_security_group_ids      = ["${data.aws_security_group.sgs.*.id}"]
+  subnet_id                   = "${data.aws_subnet.subnet.id}"
+  user_data                   = "${var.user_data}"
+  private_ip                  = "${cidrhost(var.subnet, count.index + var.start_ip)}"
+  root_block_device           = [{
+                                  delete_on_termination = true,
+                                  volume_size = "${var.os_size}"
+                                }]
+
+  tags                        = "${var.tags}"
+}
+resource "aws_volume_attachment" "repository_ec2" {
+  count         = "${var.data_disk_size} > 0 ? ${var.instance_count} : 0"
+  device_name   = "/dev/sdb"
+  volume_id     = "${element(aws_ebs_volume.repository_ec2.*.id, count.index)}"
+  instance_id   = "${element(module.repository_ec2.*.id, count.index)}"
+  force_detach  = true
+}
+resource "aws_ebs_volume" "repository_ec2" {
+  count             = "${var.data_disk_size} > 0 ? ${var.instance_count} : 0"
+  availability_zone = "${module.repository_ec2.availability_zone[0]}"
+  size              = "${var.data_disk_size}"
+}
---
+++ variables.tf
@@ -0,0 +1,56 @@
+variable "location" {
+  description = "DC area"
+}
+variable "vpc_name" {
+  description = "Ressource group name"
+}
+
+variable "instance_name" {
+  decription = "Instance name"
+}
+
+variable "instance_count" {
+  description = "Number of instance"
+  default = 1
+}
+
+variable "instance_type" {
+  description = "The instance type"
+}
+
+variable "key_name" {
+  description = "The key name to use to connect on server"
+}
+
+variable "subnet" {
+  description = "The network/subnet where to deploy instance"
+}
+
+variable "security_group_names" {
+  type = "list"
+  description = "The list of security group name to apply on instance"
+  default = ["admin"]
+}
+
+variable "user_data" {
+  description = "The cloud-init contend"
+  default = ""
+}
+
+variable "start_ip" {
+  description = "The static IP begin range"
+}
+
+variable "os_size" {
+  description = "The OS size in Go"
+  default = 8
+}
+
+variable "data_disk_size" {
+  description = "The data size disk in Go"
+  default = 0
+}
+
+variable "tags" {
+  type = "map"
+}
